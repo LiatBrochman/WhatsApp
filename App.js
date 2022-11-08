4492bb3d335b3adcb8b8f04f4845d182380209c1
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-11-08 15:48:41 +0200

    UI-4 Chats List

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
+import ChatsScreen from './src/screens/ChatsScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+            <ChatsScreen/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        paddingVertical: 50,
+    },
 });
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,55 +1,59 @@
 import {Text, View, Image, StyleSheet} from 'react-native';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
-const ChatListItem = () => {
-  return(
-      <View style={styles.container}>
-        <Image
-            source={{
-              uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
-            }}
-            style={styles.image}
-        />
+const ChatListItem = ({chat}) => {
 
-      <View style={styles.content}>
-      <View style={styles.row}>
-        <Text style={styles.name} numberOfLines={1}>Lukas</Text>
-        <Text style={styles.subTitle}>8:30</Text>
-      </View>
-        <Text numberOfLines={2} style={styles.subTitle}>Hello</Text>
-      </View>
-      </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image
+                source={{
+                    uri: chat.user.image,
+                }}
+                style={styles.image}
+            />
+
+            <View style={styles.content}>
+                <View style={styles.row}>
+                    <Text style={styles.name} numberOfLines={1}>{chat.user.name}</Text>
+                    <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
+                </View>
+                <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
+            </View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flexDirection: 'row',
-    marginHorizontal: 10,
-    marginVertical: 5,
-    height: 70,
-  },
-  image: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    marginRight: 10,
-  },
-  content:{
-    flex: 1,
-    borderBottomWidth: 1,
-    borderBottomColor: 'lightgray'
-  },
-  row:{
-    flexDirection: 'row',
-    marginBottom: 5,
-  },
-  name:{
-    flex: 1,
-    fontWeight: 'bold',
-  },
-  subTitle:{
-    color: 'gray',
-  }
+    container: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        marginVertical: 5,
+        height: 70,
+    },
+    image: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        marginRight: 10,
+    },
+    content: {
+        flex: 1,
+        borderBottomWidth: 1,
+        borderBottomColor: 'lightgray'
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 5,
+    },
+    name: {
+        flex: 1,
+        fontWeight: 'bold',
+    },
+    subTitle: {
+        color: 'gray',
+    }
 })
 
 export default ChatListItem;

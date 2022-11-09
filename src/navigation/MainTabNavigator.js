109d8988cc4294df432553b730d1e1c6a1ec3d2b
LiatBrochman <92271841+LiatBrochman@users.noreply.github.com>
2022-11-09 16:27:15 +0200

    UI-8 Contacts

--- src/navigation/MainTabNavigator.js
+++ src/navigation/MainTabNavigator.js
@@ -1,11 +1,13 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import NotImplementedScreen from "../screens/NotImplementedScreen";
 import ChatsScreen from "../screens/ChatsScreen";
-import {Ionicons, Entypo } from '@expo/vector-icons';
+import {Ionicons, Entypo} from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator()
 
 const MainTabNavigator = () => {
+
     return (
         <Tab.Navigator initalRouteName="Chats"
                        screenOptions={{
@@ -27,18 +29,19 @@ const MainTabNavigator = () => {
                 tabBarIcon: ({color, size}) =>
                     (<Ionicons name="camera-outline" size={size} color={color}/>)
             }}/>
-            <Tab.Screen name="Chats" component={ChatsScreen} options={{
+            <Tab.Screen name="Chats" component={ChatsScreen} options={({navigation}) => ({
                 tabBarIcon: ({color, size}) =>
                     (<Ionicons name="ios-chatbubbles-sharp" size={size} color={color}/>),
                 headerRight: () => (
                     <Entypo
-                        name="new-message"
-                        size={18}
-                        color={"royalblue"}
-                        style={{ marginRight: 15 }}
+                        onPress={() => navigation.navigate('Contacts')}
+                            name="new-message"
+                            size={18}
+                            color={"royalblue"}
+                            style={{marginRight: 15}}
                     />
-                )
-            }}/>
+                ),
+            })}/>
             <Tab.Screen name="Settings" component={NotImplementedScreen} options={{
                 tabBarIcon: ({color, size}) =>
                     (<Ionicons name="settings-outline" size={size} color={color}/>)
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import ChatsScreen from "../screens/ChatsScreen";
+import ContactsScreen from "../screens/ContactsScreen";
 import ChatScreen from "../screens/ChatScreen";
 import MainTabNavigator from "./MainTabNavigator";
 
@@ -12,6 +12,7 @@ const Navigator = () => {
        <Stack.Navigator screenOptions={{headerStyle: {backgroundColor: 'whitesmoke'}}}>
            <Stack.Screen name='Home' component={MainTabNavigator} options={{headerShown: false}}/>
            <Stack.Screen name='Chat' component={ChatScreen} />
+           <Stack.Screen name='Contacts' component={ContactsScreen} />
        </Stack.Navigator>
         </NavigationContainer>
     );
